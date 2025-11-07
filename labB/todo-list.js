eb16bb5859d77b77083421fa8b5bc71eaee5d706
nicku3d <nicku3d@gmail.com>
2025-11-07 19:10:20 +0100

    labb finished

--- labB/todo-list.js
+++ labB/todo-list.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const NO_ERROR = 0;
+const ERROR_NAME = 1;
+const ERROR_DATE = 2;
+
 const todoList = {
     isInit: false,
     tasks: [],
@@ -8,14 +12,17 @@ const todoList = {
 
     init: function() {
         if (!todoList.isInit) {
-            console.log('init...');
             todoList.draw();
             todoList.isInit = true;
+            const search = document.getElementById('search');
+            search.addEventListener('input', () => {
+                const searchTerm = search.value.trim();
+                todoList.search(searchTerm);
+            });
         }
     },
 
     deleteTask: function (id) {
-        console.log('DELETE ID:' + id);
         this.tasks.splice(id, 1);
         this.draw();
     },
@@ -23,43 +30,76 @@ const todoList = {
     addTask: function () {
         const taskName = document.getElementById('task');
         const deadline = document.getElementById('deadline');
-        //todo clear deadline input after add i guess?
-        console.log('ADD:' + taskName.value + " " + deadline.value);
-        this.tasks.push({name: taskName.value, deadline: deadline.value});
+        const errorNumber = this.validateAndGetErrorNumber(taskName.value, deadline.value);
+        this.toggleError(errorNumber);
+        if (errorNumber == NO_ERROR) {
+            // if no error then task can be added
+            this.tasks.push({name: taskName.value, deadline: deadline.value});
+            //clear input
+            document.getElementById('task').value = '';
+            document.getElementById('deadline').value = '';
+        }
+
         this.draw();
     },
 
     editTask: function (e, id) {
-        //todo kliknięcie na dowolną pozycję listy zmienia ją w pole edycji; kliknięcie poza pozycję listy zapisuje zmiany
-        console.log('EDIT ID:' + id);
-        const taskNode = e.target.parentNode;
-        console.log(taskNode);
+        const taskNode = document.getElementById('task' + id);
+        taskNode.className = 'task editing';
         taskNode.innerText = '';
+
         const inputName = document.createElement('input');
         inputName.type='text';
-        inputName.id='edit-name';
+        inputName.id='edit-name' + id;
+        inputName.value = this.tasks[id].name;
         taskNode.append(inputName);
 
         const inputDeadline = document.createElement('input');
         inputDeadline.type='date';
-        inputDeadline.id='edit-deadline';
+        inputDeadline.id='edit-deadline' + id;
+        inputDeadline.value = this.tasks[id].deadline;
         taskNode.append(inputDeadline);
 
         const saveEditButton = document.createElement('button');
-        saveEditButton.text = 'zapisz edycje';
-        //todo mozna dodac guzik anulujacy edycje - proste - po prostu robi draw()
+        saveEditButton.innerText = 'zapisz';
         saveEditButton.onclick = function () {
-            const name = document.getElementById('edit-name');
-            const deadline = document.getElementById('edit-deadline');
+            const name = document.getElementById('edit-name' + id);
+            const deadline = document.getElementById('edit-deadline' + id);
             todoList.editTaskValues(id, name.value, deadline.value);
         };
+
+        const cancelEditButton = document.createElement('button');
+        cancelEditButton.innerText = 'anuluj'
+        cancelEditButton.onclick = function () {
+            todoList.draw();
+        }
+
         taskNode.append(saveEditButton);
+        taskNode.append(cancelEditButton);
     },
 
-    isTaskDataValid(name, deadline) {
-        //todo co zrobic jesli cos jest nie tak? jak wyswietlic komunikat
-        if (name.length < 3 && name.length > 255) {
-            return false;
+    toggleError(errorCode = 0) {
+        const warningElement = document.getElementById('warning');
+        switch (errorCode) {
+            case ERROR_DATE:
+                warningElement.innerText = 'Data musi być pusta albo w przyszłości.';
+                warningElement.style.display = 'block';
+                break;
+            case ERROR_NAME:
+                warningElement.innerText = 'Zadanie musi mić co najmniej 3 znaki i nie więcej niż 255 znaków.';
+                warningElement.style.display = 'block';
+                break;
+            case NO_ERROR:
+            default:
+                warningElement.innerText = '';
+                warningElement.style.display = 'none';
+                break;
+        }
+    },
+
+    validateAndGetErrorNumber(name, deadline) {
+        if (name.length < 3 || name.length > 255) {
+            return ERROR_NAME;
         }
 
         if (deadline) {
@@ -68,16 +108,21 @@ const todoList = {
             today.setHours(0,0,0,0);
             deadlineDate.setHours(0,0,0,0);
 
-            return deadlineDate >= today;
+            return deadlineDate >= today ? NO_ERROR: ERROR_DATE;
         }
 
-        return true;
+        return NO_ERROR;
     },
 
     editTaskValues: function(id, name, deadline) {
-        const taskToEdit = this.tasks[id];
-        taskToEdit.name = name;
-        taskToEdit.deadline = deadline;
+        const errorNumber = this.validateAndGetErrorNumber(name, deadline);
+        this.toggleError(errorNumber);
+
+        if (errorNumber == NO_ERROR) {
+            const taskToEdit = this.tasks[id];
+            taskToEdit.name = name;
+            taskToEdit.deadline = deadline;
+        }
         this.draw();
     },
 
@@ -101,6 +146,10 @@ const todoList = {
     createTaskElement: function(parent, id, name, deadline) {
         const task = document.createElement('div');
         task.className = 'task';
+        task.id = 'task' + id;
+
+        const valuesContainer = document.createElement('span');
+        valuesContainer.className = 'values-container';
 
         const textElement = document.createElement('span');
         textElement.className = 'task-name';
@@ -109,22 +158,37 @@ const todoList = {
         } else {
             textElement.innerText = name;
         }
-        task.append(textElement);
+        valuesContainer.append(textElement);
+        // task.ondblclick = function(e) {
+        //     // if (e.target.matches('.task')) {
+        //     //     todoList.editTask(e, id)
+        //     // }
+        // };
+        // // task.onblur = function(e) {
+        // //     task.innerText = '';
+        // // };
 
         const deadlineElement = document.createElement('span');
         deadlineElement.className = 'deadline';
         deadlineElement.innerText = deadline;
-        task.append(deadlineElement);
+        valuesContainer.append(deadlineElement);
+
+        task.append(valuesContainer);
+
+        const btnContainer = document.createElement('span');
+        btnContainer.className = 'btn-container'
 
         const editButton = document.createElement('button');
-        editButton.innerText = 'edit';
+        editButton.innerText = 'edytuj';
         editButton.onclick = function(e) { todoList.editTask(e, id) };
-        task.append(editButton);
+        btnContainer.append(editButton);
 
         const deleteButton = document.createElement('button');
-        deleteButton.innerText = 'delete';
+        deleteButton.innerText = 'usuń';
         deleteButton.onclick = function() { todoList.deleteTask(id) };
-        task.append(deleteButton);
+        btnContainer.append(deleteButton);
+
+        task.append(btnContainer);
 
         parent.append(task);
     },
@@ -141,19 +205,10 @@ const todoList = {
             return task.name.includes(todoList.searchTerm);
         };
         this.filteredTasks = this.tasks.filter(filterFunction);
-        console.debug(this.filteredTasks);
         this.draw();
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    todoList.init();
-    //todo przeniesc wszystko po init do init xd
-    const search = document.getElementById('search');
-    search.addEventListener('input', () => {
-        const searchTerm = search.value.trim();
-        todoList.search(searchTerm);
-    });
-});
+document.addEventListener('DOMContentLoaded', todoList.init);
 
 
